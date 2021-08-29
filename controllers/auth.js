73088eaa435b2c809893cf65d2aac215e7d735d7
Bartek <bartlomiej.proczkowski@gmail.com>
2021-08-29 22:54:44 +0200

    Initial & Auth

---
+++ controllers/auth.js
@@ -0,0 +1,53 @@
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { name, email, password } = req.body;
+
+    if (!name) {
+      return res.status(400).send("Name is required");
+    }
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send("Password is required and should be min 6 characters long");
+    }
+    let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send("Email is taken");
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    // console.log("Saved user", user);
+    return res.json({ ok: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Error. Try again");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found");
+    const match = await comparePassword(password, user.password);
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    user.password = undefined;
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // only works on https
+    });
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
